0952715b09d68896c42cd99b5b15f95faa0825a5
Frederik Gheysels <frederik.gheysels@telenet.be>
2021-09-01 09:02:01 +0200

    Add project files.

---
+++ ConfigurationManagerFunctionApp/Function1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Configuration;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigurationManagerFunctionApp
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            throw new ConfigurationErrorsException("Some test");
+        }
+    }
+}
